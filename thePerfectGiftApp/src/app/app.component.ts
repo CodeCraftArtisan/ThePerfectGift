1fbf8ca5266cf2653c43c89e4c6ea0db02468dfa
TodereanRaul <raultoderean20@gmail.com>
2025-03-26 14:38:40 +0100

    setup tailwind/flowbite for front

--- thePerfectGiftApp/src/app/app.component.ts
+++ thePerfectGiftApp/src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,10 @@ import { FooterComponent } from "./components/footer/footer.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'The Perfect Gift';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
